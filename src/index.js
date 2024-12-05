ab364a4080cdd1470ed90ecbc82eb058e9b7cb94
SeniorProgram <victorquichica@gmail.com>
2024-12-05 17:19:29 -0500

    Primera versión del proyecto

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 // src/index.js
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssBaseline, AppBar, Toolbar, Typography, Box, Container, Button, Grid, Paper } from '@mui/material';
+import { CssBaseline, AppBar, Toolbar, Button, Box, Container, Grid, Paper, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
-import logo from './imagen/logo.png'; // Importa el logo
-
+import logo from './imagen/logo.jpg';
+import banner from './imagen/baner.jpg';
 
 // Definir el tema
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#e64a19', // Naranja
+      main: '#00008B', // Azul oscuro
     },
     secondary: {
-      main: '#ff6b6b', // Rojo
+      main: '#FF0000', // Rojo
     },
     background: {
-      default: '#ffffff', // Blanco
+      default: '#FFFFFF', // Blanco
     },
   },
   typography: {
@@ -25,7 +25,7 @@ const theme = createTheme({
   },
 });
 
-const DistribuidoraDashboard = () => {
+const MinimarketDashboard = () => {
   const [activeSection, setActiveSection] = useState('inicio');
 
   const handleSectionChange = (section) => {
@@ -35,14 +35,11 @@ const DistribuidoraDashboard = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position="static" style={{ backgroundColor: '#e64a19' }}>
+      <AppBar position="static" style={{ backgroundColor: '#00008B' }}>
         <Toolbar>
-          <img src={logo} alt="Logo" style={{ height: '60px', marginRight: '16px' }} /> {/* Ajusta el tamaño del logo aquí */}
-          <Button color="inherit" onClick={() => handleSectionChange('inicio')}>Tiendas</Button>
-          <Button color="inherit" onClick={() => handleSectionChange('productos')}>Productos</Button>
+          <img src={logo} alt="Logo" style={{ height: '60px', marginRight: '16px' }} />
+          <Button color="inherit" onClick={() => handleSectionChange('inicio')}>Inicio</Button>
           <Button color="inherit" onClick={() => handleSectionChange('nosotros')}>Nosotros</Button>
-          <Button color="inherit" onClick={() => handleSectionChange('aliados')}>Aliados</Button>
-          <Button color="inherit" onClick={() => handleSectionChange('trabaja')}>Trabaja con nosotros</Button>
           <Button color="inherit" onClick={() => handleSectionChange('contacto')}>Contacto</Button>
           <Button color="inherit" onClick={() => handleSectionChange('analisis')}>Análisis de Ventas</Button>
         </Toolbar>
@@ -51,55 +48,79 @@ const DistribuidoraDashboard = () => {
         <Box mt={4}>
           {activeSection === 'inicio' && (
             <div>
-              <img src={`${process.env.PUBLIC_URL}/banner.jpg`} alt="Banner" style={{ width: '100%', borderRadius: '10px' }} />
-            </div>
-          )}
-          {activeSection === 'productos' && (
-            <div>
-              <h2>Productos</h2>
-              <p>Aquí puedes mostrar tus productos.</p>
+              <img src={banner} alt="Banner" style={{ width: '100%', borderRadius: '10px' }} />
+              <Typography variant="h4" style={{ marginTop: '20px', textAlign: 'center' }}>
+                ¡Bienvenidos a Minimarket Panchito!
+              </Typography>
+              <Typography variant="body1" style={{ marginTop: '10px', textAlign: 'center' }}>
+                Aquí encontrarás dashboards de información sobre los reportes de ventas realizadas a cada proveedor. Nuestra misión es proporcionar datos precisos y útiles para la toma de decisiones.
+              </Typography>
+              <Grid container justifyContent="center" spacing={2} style={{ marginTop: '20px' }}>
+                <Grid item xs={12} md={4}>
+                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #00008B', borderRadius: '10px', backgroundColor: '#FFF700' }}>
+                    <Typography variant="h5">Dashboard de Ventas Anuales</Typography>
+                    <Typography variant="body1">
+                      Visualiza el resumen de ventas realizadas cada año.
+                    </Typography>
+                    <Button variant="contained" color="primary" style={{ marginTop: '10px' }} onClick={() => handleSectionChange('analisis')}>
+                      Ver Dashboard
+                    </Button>
+                  </Paper>
+                </Grid>
+              </Grid>
             </div>
           )}
           {activeSection === 'nosotros' && (
             <div>
               <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #e64a19', borderRadius: '10px' }}>
-                    <Typography variant="h4" style={{ color: '#e64a19', marginBottom: '20px' }}>MISIÓN</Typography>
+                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #00008B', borderRadius: '10px', backgroundColor: '#FFF700' }}>
+                    <Typography variant="h4" style={{ color: '#00008B', marginBottom: '20px' }}>MISIÓN</Typography>
                     <Typography variant="body1">
                       Nos dedicamos a la distribución y comercialización de una amplia variedad de abarrotes de alta calidad, que reflejan nuestra pasión por la satisfacción de nuestro país. Nuestra operación está respaldada por la certificación HACCP de seguridad alimentaria, garantizando que nuestros productos cumplen con los más altos estándares de calidad y seguridad para nuestros clientes.
                     </Typography>
-                    <img src={`${process.env.PUBLIC_URL}/mision.jpg`} alt="Misión" style={{ width: '100%', borderRadius: '10px', marginTop: '20px' }} />
+                    <img src={`${process.env.PUBLIC_URL}/imagen/mision.jpg`} alt="Misión" style={{ width: '100%', borderRadius: '10px', marginTop: '20px' }} />
                   </Paper>
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #e64a19', borderRadius: '10px' }}>
-                    <Typography variant="h4" style={{ color: '#e64a19', marginBottom: '20px' }}>VISIÓN</Typography>
+                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #00008B', borderRadius: '10px', backgroundColor: '#FFF700' }}>
+                    <Typography variant="h4" style={{ color: '#00008B', marginBottom: '20px' }}>VISIÓN</Typography>
                     <Typography variant="body1">
                       Aspiramos a ser líderes en la industria de distribución de abarrotes, siendo la primera opción para nuestro público objetivo. Buscamos mantener vínculos duraderos con clientes y proveedores basados en la confianza y beneficio mutuo. Nuestra meta es enriquecer la vida de las familias peruanas con nuestros productos, contribuyendo al bienestar comunitario y desarrollo sostenible.
                     </Typography>
-                    <img src={`${process.env.PUBLIC_URL}/vision.jpg`} alt="Visión" style={{ width: '100%', borderRadius: '10px', marginTop: '20px' }} />
+                    <img src={`${process.env.PUBLIC_URL}/imagen/vision.jpg`} alt="Visión" style={{ width: '100%', borderRadius: '10px', marginTop: '20px' }} />
                   </Paper>
                 </Grid>
               </Grid>
             </div>
           )}
-          {activeSection === 'aliados' && (
-            <div>
-              <h2>Aliados</h2>
-              <p>Aquí puedes mostrar tus aliados.</p>
-            </div>
-          )}
-          {activeSection === 'trabaja' && (
-            <div>
-              <h2>Trabaja con nosotros</h2>
-              <p>Aquí puedes mostrar oportunidades de empleo.</p>
-            </div>
-          )}
           {activeSection === 'contacto' && (
             <div>
               <h2>Contacto</h2>
-              <p>Aquí puedes mostrar información de contacto.</p>
+              <Grid container spacing={2} style={{ marginTop: '20px' }}>
+                <Grid item xs={12} md={6}>
+                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #00008B', borderRadius: '10px', backgroundColor: '#FFF700' }}>
+                    <Typography variant="h5">Gonzalo Chuecas</Typography>
+                    <Typography variant="body1">
+                      Desarrollador
+                    </Typography>
+                    <Typography variant="body1">
+                      Correo: gonzalo.chuecas@example.com
+                    </Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Paper style={{ padding: '20px', textAlign: 'center', border: '2px solid #00008B', borderRadius: '10px', backgroundColor: '#FFF700' }}>
+                    <Typography variant="h5">Milagro Arango</Typography>
+                    <Typography variant="body1">
+                      Desarrollador
+                    </Typography>
+                    <Typography variant="body1">
+                      Correo: milagro.arango@example.com
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
             </div>
           )}
           {activeSection === 'analisis' && (
@@ -107,13 +128,27 @@ const DistribuidoraDashboard = () => {
               <h2>Análisis de Ventas</h2>
               <iframe
                 title="Análisis de Ventas"
-                src="https://app.powerbi.com/view?r=eyJrIjoiMjhkYjQyZTAtNTg1Ni00NjgyLWE2NmQtN2MwZjRlMzVhMjIwIiwidCI6IjEzODQxZDVmLTk2OGQtNDYyNC1hN2RhLWQ2OGE2MDA2YTg0YSIsImMiOjR9"
+                src="https://app.powerbi.com/view?r=eyJrIjoiYzhmNTI0M2ItZmEwNi00NzhjLWE4OGQtNzk2ODE0MTM0ZDllIiwidCI6IjEzODQxZDVmLTk2OGQtNDYyNC1hN2RhLWQ2OGE2MDA2YTg0YSIsImMiOjR9"
                 style={{ width: '100%', height: '600px', border: 'none', borderRadius: '10px' }}
               ></iframe>
             </div>
           )}
         </Box>
       </Container>
+      <Box mt={4} p={2} style={{ backgroundColor: '#00008B', color: '#FFFFFF', textAlign: 'center' }}>
+        <Typography variant="body1">
+          Desarrollado por Gonzalo Chuecas y Milagro Arango - 2023
+        </Typography>
+        <Typography variant="body1">
+          Información de Contacto:
+        </Typography>
+        <Typography variant="body1">
+          Gonzalo Chuecas: gonzalo.chuecas@example.com
+        </Typography>
+        <Typography variant="body1">
+          Milagro Arango: milagro.arango@example.com
+        </Typography>
+      </Box>
     </ThemeProvider>
   );
 };
@@ -121,7 +156,7 @@ const DistribuidoraDashboard = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DistribuidoraDashboard />
+    <MinimarketDashboard />
   </React.StrictMode>
 );
 
